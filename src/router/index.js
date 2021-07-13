0ad2efe68ff69d06c14e57eedbf2295095b8237c
WBC 2012 <wbc2012@WBCs-MacBook-Pro.local>
2021-07-13 16:42:13 -0500

    protegiendo el inicio

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import {auth} from '../firebase'
 
 
 Vue.use(VueRouter)
@@ -16,7 +17,8 @@ const routes = [
         path: '/',
         name: 'Inicio',
         component: () =>
-            import ( /* webpackChunkName: "inicio" */ '../views/Inicio.vue')
+            import ( /* webpackChunkName: "inicio" */ '../views/Inicio.vue'),
+        meta: { requiresAuth: true }
     },
     {
         path: '/acceso',
@@ -30,6 +32,24 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes
+})
+
+router.beforeEach((to,from,next)=>{
+    if(to.matched.some(record => record.meta.requiresAuth)){
+
+        const usuario = auth.currentUser;
+        console.log(usuario);
+
+        if(!usuario){
+            
+            next({path:'/acceso'});
+        }else{
+
+            next();
+        }
+    }else{
+        next();//->llevalo a esa ruta
+    }
 })
 
 export default router
