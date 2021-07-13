d029323b701eebab40bd0f9e15c14a03ddf0524b
DavidSC <david@wbcboxing.com>
2021-07-13 03:10:13 -0500

    first commit

---
+++ src/views/Acceso.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1>Acceos de usuarios</h1>
+        <form @submit.prevent="ingresoUsuario({email:email,password:pass})">
+            <input 
+                type="email" 
+                placeholder="Ingrese email"
+                v-model="email"
+            >
+
+            <input 
+                type="password" 
+                placeholder="Ingrese password"
+                v-model="pass"
+            >
+
+            <button type="submit">Acceder</button>
+        </form>
+        <p>{{error}}</p>
+    </div>
+</template>
+
+<script>
+import { mapActions,mapState} from 'vuex'
+export default {
+    name:'Acceso',
+    data() {
+        return {
+            email: '',
+            pass:''
+        }
+    },
+    methods:{
+        ...mapActions(['ingresoUsuario'])
+    },
+    computed:{
+        ...mapState(['error'])
+    }
+}
+</script>
