1d394bb51d98486e6fc59845feb2bbaa7ddded89
WBC 2012 <wbc2012@WBCs-MacBook-Pro.local>
2021-07-15 13:20:25 -0500

    rutas y getters con el usuario registrado

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Inicio</router-link> |
-      <router-link to="/registro">Registro</router-link> |
-      <router-link to="/acceso">Acceso</router-link> |
-      <button @click="cerrarSesion">Cerrar sesion</button>
+      <router-link to="/" v-if="existeUsuario">Inicio</router-link> |
+      <router-link to="/registro" v-if="!existeUsuario">Registro</router-link> |
+      <router-link to="/acceso" v-if="!existeUsuario">Acceso</router-link> |
+      <button @click="cerrarSesion" v-if="existeUsuario">Cerrar sesion</button>
     </div>
     <router-view/>
   </div>
 </template>
 <script>
-import {mapActions} from 'vuex'
+import {mapActions, mapGetters} from 'vuex'
 export default {
   methods:{
       ...mapActions(['cerrarSesion'])
-  }
+  }, computed: {
+    
+      ...mapGetters(['existeUsuario'])
+    
+  },
 }
 </script>
 <style>
--- src/store/index.js
+++ src/store/index.js
@@ -65,5 +65,14 @@ export default new Vuex.Store({
                 commit('setUsuario',usuario)
         }
     },
+    getters:{
+       existeUsuario(state){
+           if(state.usuario === null){
+               return false;
+           }else{
+               return true;
+           }
+       }
+    },
     modules: {}
 })
